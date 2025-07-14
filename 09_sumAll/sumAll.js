dc5eab5a44cbb08523bb8a6d8f45d7211740fc8b
Gabriel Yamaguchi <106696472+GYamaguchiC@users.noreply.github.com>
2025-07-14 22:11:34 +0900

    add function that sum all elements between

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if(typeof start != "number" || typeof end != "number"){
+        return "ERROR"
+    }
+    if(!Number.isInteger(start) || !Number.isInteger(end)){
+        return "ERROR"
+    }
 
+    if(start < 1 || end < 1){
+        return "ERROR"
+    }
+
+    let sum = 0;
+
+    if( start > end){
+        [start, end] = [end, start]
+    }
+
+    for(let i = start; i <= end; i++){
+        sum += i;
+    }
+
+    return sum
 };
 
 // Do not edit below this line
